97369c0f3fe2f969cd6e516b906586cc9c1dafb0
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-16 12:30:11 +0200

    Load port from dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello Server!');
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}!`);
+});
