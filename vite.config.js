3d572e6b31eb39bc8be458cd43905bbff4af8a15
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-24 14:10:09 +0200

    install currently

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { PORT = 3001 } = process.env;
 
 export default defineConfig({
   plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist/app',
   },
